7823a50f0c19d833fe208dd5f0f8bb903f0aedd9
Administrator <2478794031@qq.com>
2019-09-03 08:44:15 +0800

    uih

---
+++ app/src/main/java/com/example/helloworld/SplashActivity.java
@@ -0,0 +1,41 @@
+package com.example.helloworld;
+
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashActivity extends AppCompatActivity {
+    private TextView tvVersion; //控件對象
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        //1.获取需要的控件
+        tvVersion = findViewById(R.id.tv_version);
+        //2.获取或设置控件的值
+        String version = tvVersion.getText().toString();
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(getPackageName(),0);
+            tvVersion.setText("版本号：" + info.versionName);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        //3.跳转页面
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this,SplashActivity.class);
+                startActivity(intent);
+            }
+        };
+        timer.schedule(task,5000);
+
+    }
+}
